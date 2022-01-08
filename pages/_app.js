bc98739fe4bb500906eae9cb6ae4c329386a7628
yash gupta <yash3144@gmail.com>
2022-01-08 23:18:13 +0530

    Code cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2022 Yash</p>
+      </footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
