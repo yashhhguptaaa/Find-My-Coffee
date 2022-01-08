3342a44c6fd1054a0d95b808d014d1f9c7a33d5c
yash gupta <yash3144@gmail.com>
2022-01-08 23:44:18 +0530

    Set application background

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,7 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
       <Component {...pageProps} />
-      <footer>
-        <p>© 2022 Yash</p>
-      </footer>
-    </div>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,6 @@ export default function Home() {
         <h1 className={styles.title}>Coffee Connoisseur</h1>
       </main>
 
-      <footer className={styles.footer}>
-        
-      </footer>
     </div>
   )
 }
