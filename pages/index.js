489d5989fff5950ecb205c59acfceb1a37b3558b
yash gupta <yash3144@gmail.com>
2022-01-13 22:38:51 +0530

    fETCH data from foursquare

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,23 @@ import Card from "../components/card";
 import coffeeStoresData from "../data/coffee-stores.json";
 
 export async function getStaticProps(context) {
-  console.log("contexT:", context);
+  
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `${process.env.FOURSQUARE_CLIENT_ID}`
+    }
+  };
+  
+  
+  const response = await fetch('https://api.foursquare.com/v3/places/nearby?ll=19.082197839427938%2C72.87293569719792&query=Coffee&limit=30', options)
+  const data = await response.json()
+  console.log("ssss",data.results)
+
   return {
     props: {
-      coffeeStores: coffeeStoresData,
+      coffeeStores: data.results,
     },
   };
 }
@@ -46,7 +59,7 @@ export default function Home(props) {
                 <Card
                   key={coffeeStore.id}
                   name={coffeeStore.name}
-                  imgUrl={coffeeStore.imgUrl}
+                  imgUrl={coffeeStore.imgUrl || 'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'}
                   href={`/coffee-store/${coffeeStore.id}`}
                   className={styles.card}
                 />
