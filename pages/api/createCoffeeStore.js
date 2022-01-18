e84d76686cfe0d02c5286b3265a55e5dbff0da95
yash gupta <yash3144@gmail.com>
2022-01-18 22:27:05 +0530

    working on createCoffeeStore API

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -3,4 +3,37 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.AIRTABLE_BASE_KEY
 );
 
-const table = base('coffee-stores');
+const table = base("coffee-stores");
+
+const createCoffeeStore = async (req, res) => {
+  if (req.method === "POST") {
+    // find a record
+    try {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="0"`,
+        })
+        .firstPage();
+
+      console.log({ findCoffeeStoreRecords });
+
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      } else {
+        // create a record
+        res.json({ message: "Create a record" });
+      }
+    } catch (error) {
+
+      console.log("Error finding store", error);
+      res.status(500).json({message: "Error finding store",error})
+    }
+  }
+};
+
+export default createCoffeeStore;
